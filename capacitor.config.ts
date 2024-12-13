37b86089cae99c01aed14b664d4d984be013905d
nihaadk <nihad.keric@emineo.ch>
2024-12-13 08:08:07 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demo_user',
+  webDir: 'www'
+};
+
+export default config;
